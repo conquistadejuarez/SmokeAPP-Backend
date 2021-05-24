9292c92b4abb08b0de130b2feb946ae9ce700fda
Milos <lesicsgo123@hotmail.com>
2021-05-24 13:10:48 +0200

    fix

--- users_api/__init__.py
+++ users_api/__init__.py
@@ -12,6 +12,7 @@ import uuid
 def today():
     return datetime.today()
 
+
 async def __str__(id_user: uuid.UUID):
     user = await models.User.filter(id=id_user).get_or_none()
 
@@ -27,7 +28,10 @@ async def days_since_user_quits(id_user: uuid.UUID):
     if not user:
         return {'status': 'error', 'id_error': 'USER_NOT_FOUND'}
 
-    return user.days_since_user_quits
+    if user.days_since_user_quits > 0:
+        return user.days_since_user_quits
+
+    return 0
 
 
 async def calc_money_spend_per_day(id_user: uuid.UUID):
@@ -36,7 +40,9 @@ async def calc_money_spend_per_day(id_user: uuid.UUID):
     if not user:
         return {'status': 'error', 'id_error': 'USER_NOT_FOUND'}
 
-    return await user.calc_money_spend_per_day()
+    if await user.calc_money_spend_per_day() > 0:
+        return await user.calc_money_spend_per_day()
+    return 0
 
 
 async def calc_money_not_spend(id_user: uuid.UUID):
@@ -45,7 +51,9 @@ async def calc_money_not_spend(id_user: uuid.UUID):
     if not user:
         return {'status': 'error', 'id_error': 'USER_NOT_FOUND'}
 
-    return await user.calc_money_not_spend()
+    if await user.calc_money_not_spend() > 0:
+        return await user.calc_money_not_spend()
+    return 0
 
 
 async def calc_cigarettes_user_did_not_smoke(id_user):
@@ -53,5 +61,6 @@ async def calc_cigarettes_user_did_not_smoke(id_user):
 
     if not user:
         return {'status': 'error', 'id_error': 'USER_NOT_FOUND'}
-
-    return user.calc_cigarettes_user_did_not_smoke()
+    if user.calc_cigarettes_user_did_not_smoke() > 0:
+        return user.calc_cigarettes_user_did_not_smoke()
+    return 0
